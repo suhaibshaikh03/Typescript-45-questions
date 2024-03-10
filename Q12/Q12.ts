f329c8981781273c970be95c76fac11d4352776f
suhaibshaikh03 <160228973+suhaibshaikh03@users.noreply.github.com>
2024-03-10 07:51:45 -0700

    Add files via upload

---
+++ Q12/Q12.ts
@@ -0,0 +1,8 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names:string[]=["Suhaib","Ahmed","Ali","Hamza","Bilal"]
+let message:string="Greetings, How are you ";
+console.log(message + names[0])
+console.log(message + names[1])
+console.log(message + names[2])
+console.log(message + names[3])
+console.log(message + names[4])
