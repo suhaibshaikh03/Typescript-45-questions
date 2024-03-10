cd62b6f8369febc41d7ecdd89917a8dddc38235a
suhaibshaikh03 <160228973+suhaibshaikh03@users.noreply.github.com>
2024-03-10 07:49:42 -0700

    Add files via upload

---
+++ Q9/Q9.js
@@ -0,0 +1,4 @@
+//Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favouriteNum = 18;
+var message = "\"My favourite Number is ".concat(favouriteNum, "\"");
+console.log(message);
---
+++ Q9/Q9.ts
@@ -0,0 +1,5 @@
+//Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favouriteNum:number=18
+let message:string=`"My favourite Number is ${favouriteNum}"`
+console.log(message)
+
