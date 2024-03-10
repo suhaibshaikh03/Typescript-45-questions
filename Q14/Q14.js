1f7640ffa60be928e5a7900fb34d536868bca210
suhaibshaikh03 <160228973+suhaibshaikh03@users.noreply.github.com>
2024-03-10 07:53:30 -0700

    Add files via upload

---
+++ Q14/Q14.js
@@ -0,0 +1,6 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+var guestList = ["Ali", "Ahmed", "Hamza", "Bilal"];
+console.log(guestList[0] + " ,you are invited to dinner");
+console.log(guestList[1] + " ,you are invited to dinner");
+console.log(guestList[2] + " ,you are invited to dinner");
+console.log(guestList[3] + " ,you are invited to dinner");
