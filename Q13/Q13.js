802ca73836a3706661c434be618f5ac332374803
suhaibshaikh03 <160228973+suhaibshaikh03@users.noreply.github.com>
2024-03-10 07:53:10 -0700

    Add files via upload

---
+++ Q13/Q13.js
@@ -0,0 +1,8 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+var my_own_array = ["Motorcycle", "Train", "Aeroplane", "Car", "Bus"];
+var message3 = "I would like to travel by ";
+console.log(message3 + my_own_array[0]);
+console.log(message3 + my_own_array[1]);
+console.log(message3 + my_own_array[2]);
+console.log(message3 + my_own_array[3]);
+console.log(message3 + my_own_array[4]);
