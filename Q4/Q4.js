138e8bc4836dc7a933086ad4c048ba6c59f4c6c2
suhaibshaikh03 <160228973+suhaibshaikh03@users.noreply.github.com>
2024-03-10 07:36:57 -0700

    Add files via upload

---
+++ Q4/Q4.js
@@ -0,0 +1,4 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+var a = "He travels the fastest who travels alone.";
+console.log("Rudyard Kipling once said,\"".concat(a, "\"")); //used backticks because wanted to used double quotes inside
