b4c2fa75f699793188e0fce3fe5d18c6d9ac5779
suhaibshaikh03 <160228973+suhaibshaikh03@users.noreply.github.com>
2024-03-10 07:50:19 -0700

    Add files via upload

---
+++ Q10/Q10.js
@@ -0,0 +1,16 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Name:Muhammad Suhaib Shaikh
+//Date:9 March 2024
+//This program shows four operations performed
+console.log("Addition:");
+console.log(4 + 4);
+console.log("\nSubtraction:");
+console.log(10 - 2);
+console.log("\nMultiplication");
+console.log(4 * 2);
+console.log("\nDivision");
+console.log(16 / 2);
+//This program prints my name with and without whitespace characters
+var number3 = "\tSuhaib Shaikh\n";
+console.log("name with whitespaces:", number3);
+console.log("name without whitespaces", number3.trim());
