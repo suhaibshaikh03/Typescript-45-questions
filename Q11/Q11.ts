ca86715ca2c9f4c1ae77825f84e9956a3fd4d4c2
suhaibshaikh03 <160228973+suhaibshaikh03@users.noreply.github.com>
2024-03-10 07:51:19 -0700

    Add files via upload

---
+++ Q11/Q11.ts
@@ -0,0 +1,7 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time
+let names:string[]=["Suhaib","Ahmed","Ali","Hamza","Bilal"]
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
