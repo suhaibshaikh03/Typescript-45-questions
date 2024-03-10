cb268c3354a02345c8cb32d7ad98c772f097272f
suhaibshaikh03 <160228973+suhaibshaikh03@users.noreply.github.com>
2024-03-10 07:45:28 -0700

    Add files via upload

---
+++ Q6/Q6.ts
@@ -0,0 +1,5 @@
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let number2:string="\tSuhaib Shaikh\n"
+console.log("name with whitespaces:",number2)
+console.log("name without whitespaces",number2.trim())//trim is a built-in function that removes leading and trailing whitespaces from the variable number2
+
