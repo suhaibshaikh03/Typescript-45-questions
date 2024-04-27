27929348b947634770724b6d4d9bfdf22f743389
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-04-27 13:33:20 -0700

    Add files via upload

---
+++ Q22/Q22.js
@@ -0,0 +1,4 @@
+var array = ["ali", "umar", "usman", "hamza"];
+//console.log(array[5]) undefined value as it does not exist
+console.log(array[0]);
+console.log(array[3]);
---
+++ Q22/Q22.ts
@@ -0,0 +1,4 @@
+let array:string[]=["ali","umar","usman","hamza"]
+//console.log(array[5]) undefined value as it does not exist
+console.log(array[0])
+console.log(array[3])
