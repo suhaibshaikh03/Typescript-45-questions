c24f1b770e54ca55fad49b5d1bf48ae3093dafca
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-04-27 13:23:07 -0700

    Add files via upload

---
+++ Q20/Q20.js
@@ -0,0 +1,6 @@
+var x = ["America", "Canada", "Austraia", "Germany", "Saudi arabia"];
+console.log("Countries I'd like to visit:");
+var i;
+for (i = 0; i < x.length; i++) {
+    console.log(x[i]);
+}
---
+++ Q20/Q20.ts
@@ -0,0 +1,6 @@
+let x: string[] = ["America", "Canada", "Austraia", "Germany", "Saudi arabia"]
+console.log("Countries I'd like to visit:")
+let i:number;
+for(i=0;i<x.length;i++){
+    console.log(x[i]);
+}
