3cfd107a7e0d360228396523feb598f49f0f8a66
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-04-27 13:18:06 -0700

    Add files via upload

---
+++ Q19/Q19.js
@@ -0,0 +1,2 @@
+var guests = ["Ali", "hameed", "ayesha", "umar", "usman"];
+console.log("I am inviting ".concat(guests.length, " people to dinner."));
---
+++ Q19/Q19.ts
@@ -0,0 +1,2 @@
+let guests: string[] = ["Ali", "hameed", "ayesha", "umar", "usman"];
+console.log(`I am inviting ${guests.length} people to dinner.`);
