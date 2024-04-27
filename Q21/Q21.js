deae659ca3a4ea79e49ac44ec18210ff137c50d2
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-04-27 13:29:08 -0700

    Add files via upload

---
+++ Q21/Q21.js
@@ -0,0 +1,6 @@
+var abc = {
+    name: "Suhaib",
+    age: 20,
+    school: "SMCS"
+};
+console.log("your name is ".concat(abc.name, " .Your age is ").concat(abc.age, " and you studied in ").concat(abc.school, " school"));
---
+++ Q21/Q21.ts
@@ -0,0 +1,7 @@
+let abc:{ name:string;age:number;school:string }=
+{
+name:"Suhaib",
+age:20,
+school:"SMCS"
+}
+console.log(`your name is ${abc.name} .Your age is ${abc.age} and you studied in ${abc.school} school`)
