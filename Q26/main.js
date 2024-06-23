50c01d27b9225a5f8589dc42c9b83f7475ea7b28
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-06-23 12:21:29 -0700

    Add files via upload

---
+++ Q26/main.js
@@ -0,0 +1,15 @@
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
+// let alien_color: string = 'yellow';
+// if (alien_color === 'green') {
+//     console.log("The player just earned 5 points for shooting the alien.");
+// } else {
+//     console.log("The player just earned 10 points.");
+// }
+// if the commented code will run the else block will run
+//the output would be : The player just earned 10 points.
