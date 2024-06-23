95619abf47008d420d478249662e8d116b855df5
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-06-23 12:24:35 -0700

    Add files via upload

---
+++ Q27/main.js
@@ -0,0 +1,33 @@
+// Version 1: Alien Color is Green
+var alien_color_1 = 'green';
+if (alien_color_1 === 'green') {
+    console.log("The player just earned 5 points.");
+}
+else if (alien_color_1 === 'yellow') {
+    console.log("The player just earned 10 points.");
+}
+else if (alien_color_1 === 'red') {
+    console.log("The player just earned 15 points.");
+}
+// Version 2: Alien Color is Yellow
+var alien_color_2 = 'yellow';
+if (alien_color_2 === 'green') {
+    console.log("The player just earned 5 points.");
+}
+else if (alien_color_2 === 'yellow') {
+    console.log("The player just earned 10 points.");
+}
+else if (alien_color_2 === 'red') {
+    console.log("The player just earned 15 points.");
+}
+// Version 3: Alien Color is Red
+var alien_color_3 = 'red';
+if (alien_color_3 === 'green') {
+    console.log("The player just earned 5 points.");
+}
+else if (alien_color_3 === 'yellow') {
+    console.log("The player just earned 10 points.");
+}
+else if (alien_color_3 === 'red') {
+    console.log("The player just earned 15 points.");
+}
---
+++ Q27/main.ts
@@ -0,0 +1,32 @@
+// Version 1: Alien Color is Green
+let alien_color_1: string = 'green';
+
+if (alien_color_1 === 'green') {
+    console.log("The player just earned 5 points.");
+} else if (alien_color_1 === 'yellow') {
+    console.log("The player just earned 10 points.");
+} else if (alien_color_1 === 'red') {
+    console.log("The player just earned 15 points.");
+}
+
+// Version 2: Alien Color is Yellow
+let alien_color_2: string = 'yellow';
+
+if (alien_color_2 === 'green') {
+    console.log("The player just earned 5 points.");
+} else if (alien_color_2 === 'yellow') {
+    console.log("The player just earned 10 points.");
+} else if (alien_color_2 === 'red') {
+    console.log("The player just earned 15 points.");
+}
+
+// Version 3: Alien Color is Red
+let alien_color_3: string = 'red';
+
+if (alien_color_3 === 'green') {
+    console.log("The player just earned 5 points.");
+} else if (alien_color_3 === 'yellow') {
+    console.log("The player just earned 10 points.");
+} else if (alien_color_3 === 'red') {
+    console.log("The player just earned 15 points.");
+}
