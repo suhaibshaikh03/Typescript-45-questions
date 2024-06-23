bc7362c0b0ff7881ce9ac5b7970cd4fd856b42b8
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-06-23 12:17:32 -0700

    Add files via upload

---
+++ Q25/main.js
@@ -0,0 +1,9 @@
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points.");
+}
+// let alien_color: string = 'red';
+// if (alien_color === 'green') {
+//     console.log("The player just earned 5 points.");
+// }
+// no output if the commented code is run
---
+++ Q25/main.ts
@@ -0,0 +1,11 @@
+let alien_color: string = 'green';
+
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points.");
+}
+// let alien_color: string = 'red';
+
+// if (alien_color === 'green') {
+//     console.log("The player just earned 5 points.");
+// }
+// no output if the commented code is run
