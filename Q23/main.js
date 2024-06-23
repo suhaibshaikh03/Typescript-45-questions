e56c2b19c3ddd79c049252e25602ff72c353e4ec
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-06-23 12:10:43 -0700

    Add files via upload

---
+++ Q23/main.js
@@ -0,0 +1,25 @@
+var car = 'toyota';
+var age = 25;
+var name = 'Suhaib';
+var isLoggedIn = true;
+var score = 85;
+//True tests
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car === 'toyota'); // Use triple equals (===) for strict comparison
+console.log("Is age >= 18? I predict True.");
+console.log(age >= 18);
+console.log("Is name == 'Alice'? I predict True.");
+console.log(name === 'Suhaib');
+console.log("Is isLoggedIn == true? I predict True.");
+console.log(isLoggedIn === true);
+console.log("Is score > 80? I predict True.");
+console.log(score > 80);
+//False tests
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car === 'honda');
+console.log("Is age < 18? I predict False.");
+console.log(age < 18);
+console.log("Is name == 'Bob'? I predict False.");
+console.log(name === 'Bob');
+console.log("Is isLoggedIn == false? I predict False.");
+console.log(isLoggedIn === false);
