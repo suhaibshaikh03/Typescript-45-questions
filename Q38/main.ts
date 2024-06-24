7420f18913ba759ce3536d3526ebb9d1e5a429c2
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2024-06-23 23:03:46 -0700

    Add files via upload

---
+++ Q38/main.ts
@@ -0,0 +1,8 @@
+function describe_city(city: string, country: string = "Pakistan") 
+{
+    console.log(`${city} is in ${country}.`);
+}
+
+describe_city("Karachi");
+describe_city("Lahore");
+describe_city("Tokyo", "Japan");
